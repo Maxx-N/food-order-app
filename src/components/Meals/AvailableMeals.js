24b00fe3e9537a3cd0d0c977ce471c9aa46acbb2
Maxime Norval <mnorval@kinomap.net>
2023-08-30 20:17:26 +0200

    fetching meals with HTTP and handling the loading state and errors

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,18 +1,62 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
+
 import styles from './AvailableMeals.module.css';
-import meals from '../../assets/dummy-meals';
-import MealItem from './MealItem/MealItem';
 import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
+
+const API_URL =
+  'https://react-movies-e46e6-default-rtdb.europe-west1.firebasedatabase.app';
 
 const AvailableMeals = () => {
-  return (
-    <Card className={styles.meals}>
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        const response = await fetch(`${API_URL}/meals.json`);
+        if (!response.ok) {
+          throw Error('Fetching data failed.');
+        }
+        const data = await response.json();
+        const transformedData = Object.keys(data).map((key) => {
+          return {
+            id: key,
+            ...data[key],
+          };
+        });
+        setMeals(transformedData);
+      } catch (err) {
+        setError(
+          `Error: ${!!err.message ? err.message : 'Something went wrong.'}`
+        );
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  let content;
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  } else if (!!error) {
+    content = <p className={styles['error-message']}>{error}</p>;
+  } else {
+    content = (
       <ul>
         {meals.map((meal) => {
           return <MealItem key={meal.id} meal={meal} />;
         })}
       </ul>
-    </Card>
-  );
+    );
+  }
+
+  return <Card className={styles.meals}>{content}</Card>;
 };
 
 export default AvailableMeals;
